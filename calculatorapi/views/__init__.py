e1d8575649fefb7fa637f4c71bb02e9826044955
charlesmerriman <zacmerr@gmail.com>
2025-12-16 14:36:33 -0600

    Enabled patch functionality to update user stats

--- calculatorapi/views/__init__.py
+++ calculatorapi/views/__init__.py
@@ -3,7 +3,7 @@ from .banner_tag import BannerTagSerializer, BannerTagView
 from .banner import BannerSerializer, BannerViewSet
 from .user import UserSerializer, UserViewSet
 from .user_planned_banner import UserPlannedBannerSerializer, UserPlannedBannerViewSet
-from .team_trial_rank import TeamTrialsRankSerializer, TeamTrialsRankViewSet
+from .team_trials_rank import TeamTrialsRankSerializer, TeamTrialsRankViewSet
 from .club_rank import ClubRankSerializer, ClubRankViewSet
 from .champions_meeting_rank import (
     ChampionsMeetingRankSerializer,
